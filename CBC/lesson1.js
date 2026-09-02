10e7e9525252c7a68fda95bc37ad1339a314e894
l0rdb3n <benmaldo1@gmail.com>
2026-09-02 18:01:01 -0400

    modified:   CBC/lesson1.js

--- CBC/lesson1.js
+++ CBC/lesson1.js
@@ -1,6 +1,29 @@
 let r = 255
 let g = 0
 let b = 88
-console.log(r)
-console.log(g)
-console.log(b)
+ function printPixels (givenArray){
+   for (const i of givenArray){
+   console.log(i.R)
+   console.log(i.G)
+   console.log(i.B)
+   console.log(i.A + "\n")
+
+ }
+}
+ const pixel = (R,G,B,A) => {
+    return {
+      R,
+      G,
+      B,
+      A
+   }
+ }
+
+const array = [pixel(0,0,0,100),pixel(128,128,128,0),pixel(255,255,255,100)]
+const newArray = array.filter(monkeys=>{
+   return monkeys.R>100
+})
+printPixels(array)
+console.log("\n-----------------------------------------------------------\n")
+printPixels(newArray)
+
