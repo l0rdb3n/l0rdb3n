042ebbe807b781e4697f3a5b60282969e422bf9d
l0rdb3n <mirror@github.com>
2026-09-05 22:55:21 -0400

    modified:   CBC/script.js

--- CBC/script.js
+++ CBC/script.js
@@ -1,7 +1,13 @@
+const image = new Image()
 const imageIn = document.getElementById("imageIn")
 console.log(imageIn)
+
 imageIn.addEventListener("change",(e)=>{
     console.log(e.target)
     const image = e.target.files[0]
     console.log(image)
+})
+URL.createObjectURL(image)
+imageIn.addEventListener("load",()=>{
+    console.log("Image Loaded")
 })
